829b410b72602e75a22f29028bbf0a2ba6a757e2
Sergio Rubio <sergio@rubio.name>
2009-09-25 14:21:23 +0200

    * code cleanup

--- virtualization_guest_list.rb
+++ virtualization_guest_list.rb
@@ -1,16 +1,11 @@
-require_plugin 'ruby'
 require_plugin 'virtualization'
 
-# Build the guest list
-case languages[:ruby][:host_os]
-when /mswin/
-  # Windows dom0? :D
-else
-  if File.exists?("/proc/xen/capabilities") && File.read("/proc/xen/capabilities") =~ /control_d/i
-    virtualization[:guest_list] = []
-    from("xm list | awk '{ print $1 }' |egrep -v '^(Domain-0|Name)'").each_line do |g|
-      virtualization[:guest_list] << g
-    end
+if not virtualization.nil? and virtualization[:role] == 'host'
+  virtualization[:guest_list] = []
+  from("xm list | awk '{ print $1 }' |egrep -v '^(Domain-0|Name)'").each_line do |g|
+    virtualization[:guest_list] << g
   end
+else
+  #not a xen host
 end
 
